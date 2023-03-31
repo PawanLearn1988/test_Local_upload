73589dcfc066a8e722182424e2907a66af010dc4
PawanLearn1988 <pawan_new@yahoo.com>
2023-03-30 22:19:00 -0400

    first commit of project

---
+++ src/main/java/com/example/stockapplication/StockapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockapplicationApplication.class, args);
+	}
+
+}
